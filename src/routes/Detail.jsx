4be90c4dfb535ae7c2425286f766a9cdd2e2242f
Hyejic <hyeji0314@gmail.com>
2022-02-03 14:23:22 +0900

    detail page 작업중

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -2,6 +2,8 @@ import Axios from "axios"
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import { BASE_API_URL } from '../action/constants';
+import { DetailCont } from '../components/DetailCont';
+
 import '../css/detail.css';
 
 export const Detail = () => {
@@ -34,32 +36,37 @@ export const Detail = () => {
     return { isLoading, stories };
   };
   const { isLoading, stories } = useDataFetcher(params.id);
+  if(stories.data){
+    console.log(stories.data.id)
+  }
   
-  console.log(stories.data)
-
   return (
-    <section className="layer detail">
-      {
-        isLoading ? <div>loading...</div>
-        :
-        <div className="layer__inner detail__inner">
-          <Link to={`/`} className="btn layer__btn-back" ><span className="ir-blind">뒤로가기</span></Link>
-          {/* <button className="btn layer__btn-back" onClick={}><span className="ir-blind">뒤로가기</span></button> */}
-          <div className="detail__header">
-            <span className="user-rank super detail__user-rank">Super karma</span>
-            {/* <strong className="detail__user-name">{stories.data}</strong> */}
-            <strong className="detail__post-time">posted 15 minutes ago</strong>
-          </div>
-          <h3 className="detail__title">gives me a sense of relevance</h3>
-          <p className="detail__desc">
-            I have the habit of looking at the date of things I consume online, it gives me a sense of relevance and context, both when I'm looking for things that are "from now" but more importantly when I'm looking for things given a temporal context, for instance, programming for an old compiler, finding out how to do something with an old piece of hardware or electronics. I feel like I'm encountering more and more sites and articles where I can't seem to find the date. Google will return irrelevant results from today rather than relevant results from 10 years ago. I feel it's getting worse, is it just me?
-          </p>
-          <div className="detail__btn-wrap">
-            <button className="btn detail__btn-commont">32 commemts</button>
-            <a className="btn detail__btn-link" href="#"><span className="ir-blind">link</span></a>
-          </div>
-        </div>
-      }
-    </section>
+    isLoading 
+    ? <div>isLoading...</div> 
+    : stories.data ? <DetailCont story={stories.data} /> : null
   )
 }
+  // <section className="layer detail">
+  //   {
+  //     isLoading ? <div>loading...</div>
+  //     : stories.data ? 
+  //         <div className="layer__inner detail__inner">
+  //           <Link to={`/`} className="btn layer__btn-back" ><span className="ir-blind">뒤로가기</span></Link>
+  //           {/* <button className="btn layer__btn-back" onClick={}><span className="ir-blind">뒤로가기</span></button> */}
+  //           <div className="detail__header">
+  //             <span className="user-rank super detail__user-rank">Super karma</span>
+  //             <strong className="detail__user-name">{stories.data.by}</strong>
+  //             <strong className="detail__post-time">posted {timeForToday(stories.data.time)}</strong>
+  //           </div>
+  //           <h3 className="detail__title">{stories.data.title}</h3>
+  //           <p className="detail__desc">
+  //             I have the habit of looking at the date of things I consume online, it gives me a sense of relevance and context, both when I'm looking for things that are "from now" but more importantly when I'm looking for things given a temporal context, for instance, programming for an old compiler, finding out how to do something with an old piece of hardware or electronics. I feel like I'm encountering more and more sites and articles where I can't seem to find the date. Google will return irrelevant results from today rather than relevant results from 10 years ago. I feel it's getting worse, is it just me?
+  //           </p>
+  //           <div className="detail__btn-wrap">
+  //             <button className="btn detail__btn-commont">{stories.data.kids.length} commemts</button>
+  //             <a className="btn detail__btn-link" href={stories.data.url}><span className="ir-blind">link</span></a>
+  //           </div>
+  //         </div>
+  //       :  null
+  //   }
+  // </section>
